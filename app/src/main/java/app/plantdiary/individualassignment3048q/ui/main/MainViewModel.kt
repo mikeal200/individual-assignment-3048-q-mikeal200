91df4cae658a7ff3ba43484af224139f8f163f87
Michael Williams <willi6mc@mail.uc.edu>
2020-02-20 20:37:58 -0500

    type interface error

--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -1,10 +1,12 @@
 package app.plantdiary.individualassignment3048q.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.plantdiary.individualassignment3048q.dto.Country
 
 class MainViewModel : ViewModel() {
 
-    var countries<Arraylist<Countries>>
+    var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
 
     fun fetchCountries() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
