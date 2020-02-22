82d5d1135a0adeed4a9fa7fef4a65c5c05c6d7be
Michael Williams <willi6mc@mail.uc.edu>
2020-02-22 14:18:18 -0500

    tried to get autocomplete to work but it doesnt seem to

--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -10,6 +10,10 @@ class MainViewModel : ViewModel() {
     var countries: MutableLiveData<ArrayList<Country>> = MutableLiveData<ArrayList<Country>>()
     var countryService: CountryService = CountryService()
 
+    init {
+        fetchCountries()
+    }
+
     fun fetchCountries() {
 
     }
